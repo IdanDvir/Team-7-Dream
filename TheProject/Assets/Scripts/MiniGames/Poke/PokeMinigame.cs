a39c8ec8e6b85cb0591322ec4b7ff97846611e57
Zach Levi <zach@pixelpie.games>
2024-06-18 17:19:30 +0300

    Lets add the actual minigame too

--- TheProject/Assets/Scripts/MiniGames/Poke/PokeMinigame.cs
+++ TheProject/Assets/Scripts/MiniGames/Poke/PokeMinigame.cs
@@ -1,7 +1,37 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Utils;
+
 namespace MiniGames.Poke
 {
-    public class PokeMinigame
+    public class PokeMinigame : MinigameScreen, IPointerDownHandler
     {
-        
+        [SerializeField] private GameObject THE_EYE;
+        [SerializeField] private GameObject OWWW;
+
+        private bool isActive = false;
+
+        public override void StartScreen(StopwatchView stopWatchView)
+        {
+            base.StartScreen(stopWatchView);
+            isActive = true;
+        }
+
+        protected override void OnTimerEnd()
+        {
+            OnLose();
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (isActive)
+            {
+                THE_EYE.SetActive(false);
+                OWWW.SetActive(true);
+                OnWin();
+            }
+                
+        }
     }
 }
