bee0d623455abcb4c1b0e8295720740f42cc2661
Idan Dvir <idan@pixelpie.games>
2024-06-19 11:16:52 +0300

    Implemented victory screen.

--- TheProject/Assets/Scripts/Screens/VictoryScreen.cs
+++ TheProject/Assets/Scripts/Screens/VictoryScreen.cs
@@ -1,8 +1,28 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using Utils;
 
 public class VictoryScreen : Screen
 {
+    [SerializeField] private AudioSource pirate;
+
+    public override UniTask Show()
+    {
+        WaitBeforeLoadingScene().Forget();
+        return base.Show();
+    }
+
     public override void StartScreen(StopwatchView stopWatchView)
     {
+        
+    }
+
+    private async UniTask WaitBeforeLoadingScene()
+    {
+        pirate.Play();
+        var length = pirate.clip.length;
+        await UniTask.WaitForSeconds(length + 1.0f);
+        SceneManager.LoadScene(0);
     }
 }
